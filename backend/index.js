130ae12206841971805a924baa114fc44d5920c5
Nizara-DNS <67256107+Nishara-Sewminie@users.noreply.github.com>
2023-12-07 23:28:42 +0530

    MongoDB connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
 
-app.use("/api", (req, res, next) => {
-    res.send("Hello World!")
-})
+// app.use("/api", (req, res, next) => {
+//     res.send("Hello World!")
+// })
+//NishNish
 
-app.listen(5000);
+mongoose.connect("mongodb+srv://admin:NishNish@cluster0.g3o25kk.mongodb.net/?retryWrites=true&w=majority"
+).then(()=> app.listen(5000))
+.then(()=>
+    console.log("Connected to the Database and Listening to Localhost:5000"))
+.catch((err)=>console.log(err));
